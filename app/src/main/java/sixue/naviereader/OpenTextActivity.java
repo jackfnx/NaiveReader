af76535e92a0fd4a96ce793d2159e4f2e6a84c3c
sixue <sixue@ustc>
2016-11-21 11:21:18 +0800

    new open ui

--- app/src/main/java/sixue/naviereader/OpenTextActivity.java
+++ app/src/main/java/sixue/naviereader/OpenTextActivity.java
@@ -1,10 +1,9 @@
 package sixue.naviereader;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -13,13 +12,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OpenTextActivity extends AppCompatActivity {
 
-    private MyAdapter myAdapter;
+    private String sdcard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,47 +25,61 @@ public class OpenTextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_open_text);
 
         ListView lvFiles = (ListView) findViewById(R.id.lv_files);
-        myAdapter = new MyAdapter();
-        myAdapter.setCurrentDir(Environment.getExternalStorageDirectory());
+        MyAdapter myAdapter = new MyAdapter(Environment.getExternalStorageDirectory());
+        sdcard = Environment.getExternalStorageDirectory().getAbsolutePath();
         lvFiles.setAdapter(myAdapter);
         lvFiles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 File file = (File) view.getTag();
                 if (file != null) {
-                    if (file.isDirectory()) {
-                        myAdapter.setCurrentDir(file);
-                    } else {
-                        Intent data = new Intent();
-                        data.putExtra(Utils.INTENT_PARA_BOOKNAME, file.getName());
-                        data.putExtra(Utils.INTENT_PARA_BOOKPATH, file.getAbsolutePath());
-                        setResult(RESULT_OK, data);
-                        finish();
-                    }
+                    Intent data = new Intent();
+                    data.putExtra(Utils.INTENT_PARA_BOOKNAME, file.getName());
+                    data.putExtra(Utils.INTENT_PARA_BOOKPATH, file.getAbsolutePath().substring(sdcard.length()));
+                    setResult(RESULT_OK, data);
+                    finish();
                 }
             }
         });
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            File currentDir = myAdapter.getCurrentDir();
-            if (currentDir.compareTo(Environment.getExternalStorageDirectory()) > 0) {
-                myAdapter.setCurrentDir(currentDir.getParentFile());
-                return true;
+    private class MyAdapter extends BaseAdapter {
+        private List<File> files;
+
+        public MyAdapter(File root) {
+            this.files = new ArrayList<>();
+            startLoadFiles(root);
+        }
+
+        private void startLoadFiles(final File root) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    loadFiles(root);
+                }
+            }).start();
+        }
+
+        private void loadFiles(File dir) {
+            for (File file : dir.listFiles()) {
+                if (file.isDirectory()) {
+                    loadFiles(file);
+                } else if (file.getName().endsWith(".txt")) {
+                    final File f = file;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            files.add(f);
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
             }
         }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    private class MyAdapter extends BaseAdapter {
-        private File currentDir;
-        private File[] files;
 
         @Override
         public int getCount() {
-            return files.length;
+            return files.size();
         }
 
         @Override
@@ -87,34 +99,9 @@ public class OpenTextActivity extends AppCompatActivity {
                 view.setPadding(10, 10, 10, 10);
             }
             TextView tv = (TextView) view;
-            tv.setText(files[i].isDirectory() ? "<" + files[i].getName() + ">" : files[i].getName());
-            tv.setTag(files[i]);
+            tv.setText(files.get(i).getAbsolutePath().substring(sdcard.length()));
+            tv.setTag(files.get(i));
             return view;
-        }
-
-        void setCurrentDir(File currentDir) {
-            this.currentDir = currentDir;
-            this.files = currentDir.listFiles(new FilenameFilter() {
-                @Override
-                public boolean accept(File file, String s) {
-                    if (s.startsWith(".")) {
-                        return false;
-                    }
-                    File f = new File(file.getAbsoluteFile() + "/" + s);
-                    return f.isDirectory() || s.endsWith(".txt");
-                }
-            });
-            Arrays.sort(this.files, new Comparator<File>() {
-                @Override
-                public int compare(File f1, File f2) {
-                    return f1.getName().compareTo(f2.getName());
-                }
-            });
-            notifyDataSetChanged();
-        }
-
-        File getCurrentDir() {
-            return currentDir;
         }
     }
 }
